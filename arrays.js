edfb188bbdcee5468d954848646debb682f2f8e8
Thomas Casady <tscasady@gmail.com>
2018-02-15 03:38:10 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+function addElementToBeginningOfArray(a,b){
+  var newA = a.unshift(b)
+  return newA
+}
+
+function destructiveAddElementToBeginningOfArray(c,d){
+  return c.unshift(d)
+}
+function addelementToEndOfArray
