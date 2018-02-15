1e5e4a23e17823fb03c0525f93122f07354a318d
Thomas Casady <tscasady@gmail.com>
2018-02-15 04:10:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function destructivelyAddElementToBeginningOfArray(array,element) {
 }
 
 function addElementToEndOfArray(array, element){
-  var newArray = array.push(element)
+  var newArray = [...array, element]
   return newArray
 }
 
