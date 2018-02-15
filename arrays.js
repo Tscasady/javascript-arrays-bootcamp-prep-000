7311b3c6d5a5a58ad81337d0383b02ffd8db528a
Thomas Casady <tscasady@gmail.com>
2018-02-15 03:46:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
 function addElementToBeginningOfArray(array,element){
   var newArray = array.unshift(element)
   return newArray
+}
 
 function destructivelyAddElementToBeginningOfArray(c,d) {
   return c.unshift(d)
